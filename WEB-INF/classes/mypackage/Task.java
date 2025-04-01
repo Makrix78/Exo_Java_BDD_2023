0d0dfc0e901982248776ddc9c1b33515e0d3cf95
Makrix <140716576+Makrix78@users.noreply.github.com>
2025-04-01 09:46:31 +0200

    Update Task.java

--- WEB-INF/classes/mypackage/Task.java
+++ WEB-INF/classes/mypackage/Task.java
@@ -1,17 +1,22 @@
-package modele;
+package mypackage;
 
 public class Task {
     private String title;
     private String description;
+    private String dueDate;
+    private boolean done;
 
-    public Task(String title, String description) {
+    public Task(String title, String description, String dueDate) {
         this.title = title;
         this.description = description;
+        this.dueDate = dueDate;
+        this.done = false;
     }
 
     public String getTitle() { return title; }
     public String getDescription() { return description; }
+    public String getDueDate() { return dueDate; }
+    public boolean isDone() { return done; }
 
-    public void setTitle(String title) { this.title = title; }
-    public void setDescription(String description) { this.description = description; }
+    public void setDone(boolean done) { this.done = done; }
 }
